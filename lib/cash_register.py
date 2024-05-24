b4afc40c209e48d7aaa6584d8ff6c61fd5dae10d
Sylvia <sylviachebet03@gmail.com>
2024-05-24 16:39:59 +0300

    call instance methods inside of other instance methods

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,24 +1,32 @@
-#!/usr/bin/env python3
-
 class CashRegister:
-  def __init__(self, discount=0, total=0, items=[], last_transaction=0):
-    self.discount = discount
-    self.total = total
-    self.items = items
-    self.last_transaction = last_transaction
+    def __init__(self, discount=0):
+        self.discount = discount
+        self.total = 0
+        self.items = []
+        self.previous_transactions = []
 
-  def add_item(self, title, price, quantity=1):
-    self.items.append({'title': title, 'price': price, 'quantity': quantity})
-    self.total += price * quantity
+    def add_item(self, item, price, quantity=1):
+        self.total += price * quantity
+        for _ in range(quantity):
+            self.items.append(item)
+        self.previous_transactions.append(
+            {"item": item, "quantity": quantity, "price": price}
+        )
 
-  def apply_discount(self):
-    if self.discount > 0:
-      discount_amount = self.total * (self.discount / 100)
-      self.total -= discount_amount
-      print(f"After the discount, the total comes to ${int(self.total)}.")
-    else:
-      print("There is no discount to apply.")
+    def apply_discount(self):
+        if self.discount:
+            self.total = int(self.total * ((100 - self.discount) / 100))
+            print(f"After the discount, the total comes to ${self.total}.")
+        else:
+            print("There is no discount to apply.")
 
-  
-  
-  pass
+    def void_last_transaction(self):
+        if not self.previous_transactions:
+            return "There are no transactions to void."
+        self.total -= (
+            self.previous_transactions[-1]["price"]
+            * self.previous_transactions[-1]["quantity"]
+        )
+        for _ in range(self.previous_transactions[-1]["quantity"]):
+            self.items.pop()
+        self.previous_transactions.pop()
